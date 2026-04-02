61a02262b54807a74f4badd1167d54eff5bf1e03
Amzt-pixel <abishmaraniya@gmail.com>
2026-04-02 13:28:35 +0530

    Update app.js

--- v6/app.js
+++ v6/app.js
@@ -2,7 +2,7 @@
    DICTIONARY — app.js
    ══════════════════════════════════════ */
 
-const PASSWORD     = 'BlondeButter++';
+const PASSWORD     = 'MotherFather++';
 const CSV_URL      = 'https://raw.githubusercontent.com/Amzt-pixel/NEW-VOCAB/main/dict_demo.csv';
 const HOLD_MS      = 700;
 
